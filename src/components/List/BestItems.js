b719d047b96eee408bb7da047ec531c3517aaa14
GPontheG <gpontheg@gmail.com>
2022-08-29 16:28:23 +0900

    Add: List

---
+++ src/components/List/BestItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+import './BestItems.scss';
+
+const BestItems = () => {
+  return (
+    <Card>
+	    <BestItem />
+    </Card>
+  );
+};
+
+export default BestItems;
---
+++ src/components/List/Items.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+import "./Items.scss";
+
+const Items = () => {
+    return (
+      <Card>
+          <Item />
+      </Card>
+    );
+  };
+  
+
+export default Items;
