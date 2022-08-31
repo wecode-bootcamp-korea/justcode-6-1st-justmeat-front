472a3ba10fddd9a1d5553da02af6c33263cc47fa
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-08-31 17:06:10 +0900

    💄 Design: Signup CSS 완성

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -7,20 +7,27 @@ import LoginSuggestion from '../../components/LoginSuggestion/LoginSuggestion';
 
 function Signup() {
   return (
-    <div className="container setting-center todo">
+    <div className="signup-container setting-column">
       {/* 너비 조절을 위해 content 감싸는 div 생성 */}
-      <div className="content">
-        <div className="signup-title todo">회원가입</div>
+      <div className="signup-content">
+        <div className="signup-title setting-center">회원가입</div>
         {/* SNS 간편 로그인  */}
         <SocailLogin
           type={'signup'}
           title={'SNS 계정으로 간편하게 가입하기'}
         />{' '}
         {/* email 간편 로그인  */}
-        <span className="signup-subtitle todo">이메일로 가입하기</span>
-        <article className="sns-signup-wrap todo">
+        <span className="signup-subtitle">이메일로 가입하기</span>
+        <article className="sns-signup-wrap">
           <form>
-            <button className="btn-login-email todo">
+            <button
+              className="btn-login-email"
+              style={{
+                backgroundColor: 'white',
+                border: '1px solid red',
+                color: 'red',
+              }}
+            >
               <div>정육각 회원가입 하기</div>
             </button>
           </form>
